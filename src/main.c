320947c5e3bd47b8dfdf28920ad06c0f87cd1b4f
losdayver <losdayver@yandex.ru>
2025-10-12 18:28:13 +0300

    buildrun.sh

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "module.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void cycle(int max) {
   for (int i = 0; i < max; i++) {
@@ -7,9 +8,25 @@ void cycle(int max) {
   }
 }
 
+struct foo *getStructPtr1() {
+  struct foo *x = (struct foo *)(malloc(sizeof(struct foo)));
+  x->a = 'b';
+  x->b = 32463;
+  return x;
+}
+
+struct foo *getStructPtr2() {
+  struct foo *x = (struct foo *)(malloc(sizeof(*x)));
+  *x = (struct foo){'a', 123};
+  return x;
+}
+
 int main(void) {
-  struct foo x = {'s', 10};
+  struct foo *(*operation)() = getStructPtr2;
+  struct foo *x = operation();
   printf("Hello, World!\n");
   sayHello();
+  printf("%c %d", x->a, x->b);
+  free(x);
   cycle(10);
 }
